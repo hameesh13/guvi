1ad3afe277015f4b0780c0378ffaf0821cc7ecc1
Hameesh shanmugasundaram <33625186+hameesh13@users.noreply.github.com>
2020-04-30 14:53:12 +0530

    Update main.js

--- random/main.js
+++ random/main.js
@@ -1,25 +1,23 @@
-let button=document.createElement("button");
-button.id="random";
-button.innerHTML="Generate"
-button.setAttribute("onclick","number()");
-document.getElementsByTagName("body")[0].append(button);
-let h4=document.createElement("h4");
-h4.id="gen";
-document.getElementsByTagName("body")[0].appendChild(h4);
-function number(){
-  i=0;
-  arr=[];
-  numberGen(arr);
-  h4.innerHTML="The random number generated is "+arr.join("");
+function generate() {
+  let disp = document.getElementById("number");
+  
+  disp.innerHTML = recur2();
 }
-function numberGen(arr) {
 
-if (i >= 8)
-  return;
-let newNumber = Math.floor(Math.random() * 10) ;
-if (arr.indexOf(newNumber) < 0) {
-  arr.push(newNumber);
-  i+=1;
+
+function getRandom8digit(max) {
+  return Math.floor(Math.random() * 100000000);
 }
-numberGen(arr);
+function recur2() {
+  rand_num = getRandom8digit();
+  if (validate(rand_num)) return rand_num;
+  else return recur2();
+}
+function validate(num) {
+  str_num = new Set(num.toString().split(""));
+  if (str_num.size < 8) {
+    return false;
+  } else {
+    return true;
+  }
 }
